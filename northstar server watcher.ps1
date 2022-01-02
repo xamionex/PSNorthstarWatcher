c35c28a2fd5ee0e61033c0be6030dd566d7bfb5d
RomeoCantCode <romeoloeffel@gmail.com>
2022-01-02 03:45:51 +0100

    minor changes, config switch fix
    northstar logo
    timestamps
    other minor changes
    fixed gather logs switch
    added launcher bat

--- northstar server watcher.ps1
+++ northstar server watcher.ps1
@@ -4,16 +4,15 @@ $gamedir = "Titanfall2-" #name of your titanfall folders without number, example
 $enginerrorclosepath = "engineerrorclose.exe" # absolute or relative path to your enginerrorclose.exe
 $portarray = @(8081,8082) #auth ports you want to use for your titanfall servers, also: last number is used to detect server number Titanfall2-n (eg 8081 => Titanfall 2-1), at the moment restricted to 9 servers!
 $udpstartport = 3703 #specify startport without latest number (37031=> 3703). Dont forget to adjust your portforwarding!!! 
-# $killemptypath = "D:\Games\Origin\restart0players.exe" #obsolete
 $deletelogsafterdays = 1 #how many days until logs get deleted
 $waittimebetweenserverstarts = 15 #time in seconds before server starts, depends on your server speed. recommend values between 5-30 seconds
 $waittimebetweenloops = 15 #time in seconds after each loop of this script. also refresh rate for index.html default: 15
 $waitwithstartloopscount = 8 # after a server has been started at least wait the defined count of loops to start it again. this prevents accidental server duplicate processes default: 8 (15 second loops * 8 = 120 seconds)
 $serverbrowserenable = $true
-$serverbrowserfilepath = "index.html" #absolute path to where the index.html should be saved. You can put this in like a webserver folder to make it available online. Example: http://51.154.42.207:18880/northstar/serverbrowser/
+$serverbrowserfilepath = "index.html" #absolute path to where the index.html should be saved. 
 $restartserverhours = 4 #time in hours to force restart server (kills process) after certain uptime
 $masterserverlisturl = "https://northstar.tf/client/servers" # url path to master server server list (json format)
-$myserverfilternamearray = @("Kraber","Unnamed") #put an identifier here to count your slots for serverbrowser .html file
+$myserverfilternamearray = @("Kraber","Gun") #put an identifier here to count your slots for serverbrowser .html file
 $showuptimemonitor = $true #starts 2nd powershell process with monitor if true
 $northstarlauncherargs = "-dedicated -multiple -softwared3d11" #when launching server use those args
 $crashlogscollect = $false #$true to collect them, $false to disable
@@ -44,6 +43,61 @@ function Check-Listenport([int] $port){
 ##
 
 ##vars and stuff
+Write-Host "
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWNNXXXXXXKKKKKKKKKXNWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWNXKKKKKK0xolclox0KKKKKKXNNWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWNXK000KK0xl:,',,,,,:oxO000000KNWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWNK00O000Oko:,',,'',,'''',:okO0OOOO0KXWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMWWXK0OkOOOkxoc,''''',''''''''''',codkkOkkkOKXNWMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMWNK0Okkkkkxdl;,''''''''''''''''''''''',;coxkkxxkk0KNWMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMWX0kxxxxxxoc;,''''''''''''''''''''''''''''',;codxxxdxkOKNMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMWNX0kxdddddoc;,''''''''''''''''''''''''''''''''''',;coddddddk0XNWMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMWNKOxddoddoc:;,'''':xdllc,'''''''''''''''''',:lcodc,''''';:coodoodxO0NWMMMMMMMMMMMMMMM
+MMMMMMMMMMMMNKOxdoooooc:,'.'''''c0WWNWXk:,'''''''''',;''':xXNNNWXo'''''''',;cloooodxOXNMMMMMMMMMMMMM
+MMMMMMMMMWXOxdoooooc:,'....''''',l0WWWWWXd;'',:cllc:::'':OWWWWWXd;'''''.....',:cooooodx0XWMMMMMMMMMM
+MMMMMMMMMNOdooolc;,'........''''''ckKNWWWNOkO0XWWWNXKOkk0WWWNK0o,'''''.........',;:looodONMMMMMMMMMM
+MMMMMMMMMNOdlc;,............''''''',;cld0WMMMMMMMMMMMMMMWKxlc;,''''''''............';cloONMMMMMMMMMM
+MMMMMMMMWKo:,.............'''''''''''',ckNMMMMMMMMMMMMMMWOl;'''''''''''''.............,:oONMMMMMMMMM
+MMMMMNKOo;'.............''''''''''',;,lXWMMMMMMMMMMMMMMMMWNx,,;,'''''''''''..............,cx0NWMMMMM
+MMN0d:,................'''''''''';lO0kONMMMMMMMMMMMMMMMMMMW0ox0k:,''''''''''................';oOXWMM
+Kxc'.................''''''''''';xXWWWWMMMMMMMMMMMMMMMMMMMWWWWWW0l,'''''''''''..................;d0N
+x:'................'''''''''',,,c0WWWWWWMMMMMMMMMMMMMMMMMMWWWNNWNk:,,,'''''''''..................;dX
+WN0xc'............''''''''',,;;:kNWXXWWNXWMMMMMMMMMMMMMMWXNWWKxKWKo;:;''''''''''''...........':dOXWM
+MMMMNKxc,.......''''''''',,,,:oONW0llddc:kWMWMMMMMMMMMMWKocddc;lKWXxoc,,,''''''''''.......':d0NMMMMM
+MMMMMMMWKx:'...'''''''',,,,,,dXWNXd;,....cxk0KWMMMMWX0Oxo;....';kNWNXx;,,,,''''''''''..';oONWMMMMMMM
+MMMMMMMMMMNOc''''''''',,,,,,:ONWXOc,,,loc,..,dXWWWMNk;'.':ol;..,o0NNN0c,,,,,,''''''''':kXWMMMMMMMMMM
+MMMMMMMMMMMWx,'''''',,,,,,,,c0WN0o;;l0NWNx,.;OWWWWWWK:..:0WNKd'':xXNWKl,,,,,,,''''''''lXMMMMMMMMMMMM
+MMMMMMMMMMMWd,''''',,,,,,,,;dXWKl;;:kWWWWNOodKWWWWWWXxld0WWWWXl',:xXNXd;,,,,,,,,''''''lXMMMMMMMMMMMM
+MMMMMMMMMMMWx,''',,,,,,,,;,c0NXx:;,:0WWWWWXKKNWWWWWWNX0KNWWWWNx'';cONNOc,,,,,,,,,'''''oXMMMMMMMMMMMM
+MMMMMMMMMMMWx,'',,,,,,,,;;;xXXxc;;':0WWWWNk::xXXXXKXOc;l0NWNNXd,.,;lONKl;;;,,,,,,,,'',oXMMMMMMMMMMMM
+MMMMMMMMMMMWx,',,,,,,,;;;;oKNOc;:,.;ONNNW0:..:occccoc...lXNNNKd,.';;lOXOc;;;,,,,,,,,',oXMMMMMMMMMMMM
+MMMMMMMMMMMWx,'',,,,,;;;;cONXkc:;'.:0NNNXd'..';;;;;;'...;ONNNNk,..,::xXXx:;;;;,,,,,'',oXMMMMMMMMMMMM
+MMMMMMMMMMMWd'......';;;:o0NX0kxc'.;ONNNNx'.............:ONNNNx'.':dkOXXOl;;;;,'......lXMMMMMMMMMMMM
+MMMMMMMMMMMWd.......';;;:d0XXxlol;.,xNNNNx'.............;ONNNKc..:loodKKOo:;;;,.......lXMMMMMMMMMMMM
+MMMMMMMMMMMWd'......';;;:cokKx:'...'dXNNNx,.............:ONNNk,.....,d0koc:;;;,.......lXMMMMMMMMMMMM
+MMMMMMMMMMMWd'......';;;:::ckk:.....;o0NNKc............'oXNKx:'.....,xkc:::;;;,.......lXMMMMMMMMMMMM
+MMMMMMMMMMMWd'......,;;:::::od;.......oKNXk:'........';o0NXd'.......ckd:::::;;,.......lXMMMMMMMMMMMM
+MMMMMMMMMMMWd'......,;::::::::'.......cKNXX0o,......'lOKXXKo'.......,::::::::;,.......lXMMMMMMMMMMMM
+MMMMMMMMMMMWd'......,;:::::::,.......'dXXXXXOc'.....;kXXXXXx,.........,::::::;,.......lXMMMMMMMMMMMM
+MMMMMMMMMMMWd'......,;::::::,.........;kXXXXd,'''..',oKXXXO:..........';::::::,.......lXMMMMMMMMMMMM
+MMMMMMMMMMMWd'......,;:::::;'.........,dKXX0l;c:'..;coOXXXk;...........,::::::,'......lXMMMMMMMMMMMM
+MMMMMMMMMMMWd'......,::::::,..........;d0X0xllo:'..;lox0KKx;...........';:::::;'......lXMMMMMMMMMMMM
+MMMMMMMMMMMWd'......,:::::;'..........;xKK0xoll:'.';lodOKKk:............,:::::;'......lXMMMMMMMMMMMM
+MMMMMMMMMMMWd'......,:::::,........''.;kXK0dlll:'.';lldOKKOl,'..........';::::;'......lXMMMMMMMMMMMM
+MMMMMMMMMMMWd'......,::::;'......'.'';lOKKOdll:,''.';coOKK0dc:,'.''......';:::;'.....'lXMMMMMMMMMMMM
+MMMMMMMMMMMWXko:,'.',:::;'...'''''',:coOKK0o;,'''''''':kKK0dlcc;,''''''..',:::;'..';lxKWMMMMMMMMMMMM
+MMMMMMMMMMMMMMWN0dc,,::;,'''''''';:cdxk0KK0d,'''''''',o0KK0Okdlcc;,'''''''';::;,:okKWMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMWNKkdl;''''''',:cccoxxdoool;'''''''',coolodxocccc:;,'''''',:ok0XWMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMWXkoc;,',:cccccc:;,''''''''''''''''''',;:cccccc:;,',:lx0NMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMWNKOxoc:ccc:;,'''''''''''''''''''''''',:ccc:clok0NWMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMWN0kdl:,''''''''''''''''''''''''''',;:ldOKNMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWNKkl:,''''''''''''''''''''''',,:dOXWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWX0xl:,,','''''''''''',,:ok0XWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMN0o;,''',,,,,,,,,,:dKNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+Northstar is awesome!! https://northstar.tf
+"
+Write-Host "Thanks for using this Powershell script. If you need help just @faky me on Northstar Discord"
+write-host (get-date -Format HH:mm:ss) "Starting Northstar Server Watcher"
 $serverwaitforrestartcounterarray = @()
 $servercount = $portarray.count
 foreach($server in $portarray){ #initialize array for counter
@@ -57,18 +111,29 @@ if($showuptimemonitor){
 }
 $myfilterstring = ""
 foreach($filter in $myserverfilternamearray){$myfilterstring = $myfilterstring + $filter +", "} #generate name for myfilter to display later
+$singlequote = "'"
 ##
 
 ##Main loop
+$firstloop = $true
+write-host (get-date -Format HH:mm:ss) "Starting main loop."
 do{ 
     $serverstartdelay = 0
     foreach($port in $portarray){
         $portstring = $port.tostring()
         $servernumber = $portstring.substring(3) #only get latest number of 4 digit port
-        #Write-Host "server $servernumber serverwaitforestartcounterarray" $serverwaitforrestartcounterarray[($servernumber-1)]
+        #write-host (get-date -Format HH:mm:ss) "server $servernumber serverwaitforestartcounterarray" $serverwaitforrestartcounterarray[($servernumber-1)]
         $isrunning = Check-Listenport $port
 
         if($isrunning -eq $true){
+			if ($serverwaitforrestartcounterarray[($servernumber-1)] -gt 0){
+				Write-Host (get-date -Format HH:mm:ss) "Server $servernumber is detected running again."
+			}
+			else{
+				if($firstloop){
+					Write-Host (get-date -Format HH:mm:ss) "Server $servernumber is detected running."
+				}
+			}
             $serverwaitforrestartcounterarray[($servernumber-1)] = 0
         }
 
@@ -78,27 +143,30 @@ do{
             if($serverwaitforrestartcounterarray[($servernumber-1)] -eq 0){ ##
                 
                 if($timeout -eq $false){ # gather logfiles
-                    $getchilditemstring = "$originpath$gamedir$servernumber"+ "\R2Northstar\logs"
-                    $logfiles = Get-Childitem $getchilditemstring -File | sort -Descending LastWriteTime
-                    Copy-Item $logfiles[1].fullname $crashlogspath
-                    Write-Host "serve $servernumber crashed. Logfile copied to " $crashlogspath
+					if($crashlogscollect){
+						$getchilditemstring = "$originpath$gamedir$servernumber"+ "\R2Northstar\logs"
+						$logfiles = Get-Childitem $getchilditemstring -File | sort -Descending LastWriteTime
+						Copy-Item $logfiles[1].fullname $crashlogspath
+						write-host (get-date -Format HH:mm:ss) "Server $servernumber crashed. Logfile copied to " $crashlogspath
+					}
+					else{
+						write-host (get-date -Format HH:mm:ss) "Server $servernumber crashed."
+					}
                 }
 
-                Write-Host "starting server $servernumber time:" (get-date).hour (get-date).minute
                 $startprocessstring = "$originpath$gamedir$servernumber" + "\NorthstarLauncher.exe"
                 $argumentliststring = $northstarlauncherargs + " -port " + $udpstartport + $servernumber
-                $singlequote = "'"
-                $nspowershellcommand = "-command &{
-                    Write-host Executing startup delay for server $servernumber of $serverstartdelay seconds;
-                    sleep $serverstartdelay ; 
+                $nspowershellcommand = "-command &{ 
+                    write-host (get-date -Format HH:mm:ss) Executing startup delay for server $servernumber of $serverstartdelay seconds;
+                    sleep $serverstartdelay; 
                     start-process -WorkingDirectory $singlequote`"$originpath$gamedir$servernumber`"$singlequote $singlequote`"$originpath$gamedir$servernumber\NorthstarLauncher.exe`"$singlequote `" -argumentlist $singlequote `"$argumentliststring `" $singlequote;
-                }"
-                Write-Host "Starting server $severnumber using additional powershell process"
+                }" # Dont ask! ;-) only took me 2 hours to figure out
+                write-host (get-date -Format HH:mm:ss) "Starting server $servernumber using additional Powershell process."
                 Start-Process powershell -argumentlist $nspowershellcommand
                 $serverwaitforrestartcounterarray[($servernumber-1)] = $waitwithstartloopscount
             }
             else{
-                Write-Host "Waiting to start server $servernumber again for loops:" $serverwaitforrestartcounterarray[($servernumber-1)]
+                write-host (get-date -Format HH:mm:ss) "Waiting to start server $servernumber again for loops:" $serverwaitforrestartcounterarray[($servernumber-1)]
             }
 
         $serverwaitforrestartcounterarray[($servernumber-1)] = $serverwaitforrestartcounterarray[($servernumber-1)] -1 
@@ -108,14 +176,13 @@ do{
     
     start-process $enginerrorclosepath #send enter to window "Engine Error" to close it properly if crashed with msgbox
     sleep $waittimebetweenloops
-    #start-process $killemptypath #workaround restart not needed anymore
     $processes = get-process -name titanfall2-unpacked| select id,starttime
     $date = get-date
     $timeout = $false 
     foreach($process in $processes){
         if(($date - $process.StartTime).hours -ge $restartserverhours){
             Stop-Process $process.id
-            Write-Host "stopping server because it hit restartserverhours " $process.id
+            write-host (get-date -Format HH:mm:ss) "Stopping server because it is runnig for at least $restartserverhours hours. PID: " $process.id
             $timeout = $true #set timeout $true so we know we did kill that server and it did not crash, so it doesnt copy logfile
         }
     }
@@ -134,7 +201,7 @@ do{
             if($server.name -match $myserverfiltername){
                 $fakyplayercount = $fakyplayercount + $server.playerCount
                 $fakyslots = $fakyslots + $server.maxPlayers
-		        #Write-Host $server.name $server.playerCount "/" $server.maxPlayers $server.map
+		        #write-host (get-date -Format HH:mm:ss) $server.name $server.playerCount "/" $server.maxPlayers $server.map
             }
         }
         if($server.hasPassword -eq $false){
@@ -218,7 +285,7 @@ Other regions: $ucount / $uslots <br><br>
 "@
 		$file | Out-File -Append -Filepath $htmlpath
 
-		Remove-Variable file
+		clear-variable file
 		clear-variable fakyplayercount 
 		clear-variable playercountpublic
 		clear-variable totalslots
@@ -236,13 +303,14 @@ Other regions: $ucount / $uslots <br><br>
 }
 
     #log cleanup
-    #if(((get-date) - $logfilesdeletelastdate) -ge $deletelogsminutes){
     if((get-date) -ge $logfilesdeletelastdate.AddMinutes($deletelogsminutes)){
-        Write-Host "Checking/clearing logfiles because they haven't been cleared for $deletelogsminutes minutes or script was just started"
+        write-host (get-date -Format HH:mm:ss) "Checking/clearing logfiles because they haven't been cleared for $deletelogsminutes minutes or script was just started."
         $logfiles = get-childitem -recurse -include ('*.txt','*.dmp') -Path "$logfilespathstring"
         $logfilesdelete = $logfiles | Where-Object { $_.LastWriteTime -lt ((Get-Date).AddDays(-($deletelogsafterdays)))}
         $logfilesdelete | Remove-Item -Verbose
         $logfilesdeletelastdate = get-date
     }
+	$firstloop = $false
 }
 while($true) #execute forever
+##
